ad2ea935a9ff2a362d3bc7b764cec3b9088a4160
Bruno Lima <bruno@fidelize.com.br>
2017-06-24 20:57:35 -0300

    bug fix

--- android/app/build/generated/source/r/release/com/vendacerta/R.java
+++ android/app/build/generated/source/r/release/com/vendacerta/R.java
@@ -1806,10 +1806,12 @@ containing a value of this type.
         public static final int abc_textfield_search_activated_mtrl_alpha=0x7f02003b;
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f02003c;
         public static final int abc_textfield_search_material=0x7f02003d;
-        public static final int node_modules_reactnativeexperimentalnavigation_assets_back=0x7f02003e;
-        public static final int node_modules_reactnativerouterflux_src_back_chevron=0x7f02003f;
-        public static final int node_modules_reactnativerouterflux_src_menu_burger=0x7f020040;
-        public static final int notification_template_icon_bg=0x7f020041;
+        public static final int assets_image_logo=0x7f02003e;
+        public static final int node_modules_reactnativedatepicker_date_icon=0x7f02003f;
+        public static final int node_modules_reactnativeexperimentalnavigation_assets_back=0x7f020040;
+        public static final int node_modules_reactnativerouterflux_src_back_chevron=0x7f020041;
+        public static final int node_modules_reactnativerouterflux_src_menu_burger=0x7f020042;
+        public static final int notification_template_icon_bg=0x7f020043;
     }
     public static final class id {
         public static final int action0=0x7f0d0056;
